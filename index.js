d89497f2c9275911b0e84d4edba7998f5a7a8e6d
piousdeer <31318219+piousdeer@users.noreply.github.com>
2021-02-27 13:28:02 +0700

    Use TypeScript to build output

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function is(value, type) {
-  if (typeof type === "string") {
-    return typeof value === type;
-  }
-
-  return value instanceof type;
-};
--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+type AnyConstructor = new (...args: any[]) => any;
+export default function is<T extends AnyConstructor>(value: unknown, type: T): value is InstanceType<T>;
+
 export default function is(value: unknown, type: "string"): value is string;
 export default function is(value: unknown, type: "number"): value is number;
 export default function is(value: unknown, type: "bigint"): value is bigint;
@@ -7,5 +10,10 @@ export default function is(value: unknown, type: "undefined"): value is undefine
 export default function is(value: unknown, type: "object"): value is object;
 export default function is(value: unknown, type: "function"): value is Function;
 
-type AnyConstructor = new (...args: any[]) => any;
-export default function is<T extends AnyConstructor>(value: unknown, type: T): value is InstanceType<T>;
+export default function is(value: unknown, type: string | AnyConstructor) {
+  if (typeof type === "string") {
+    return typeof value === type;
+  }
+
+  return value instanceof type;
+};
